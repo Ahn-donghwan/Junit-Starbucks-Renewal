1cf2f185bd9541ac44a836c0c0e66e80c03d77fc
unknown <ddh1713flrwls1713@gmail.com>
2026-07-27 15:54:56 +0900

    test(coupon): saveCouponSuccess

--- src/test/java/com/team114/starbucks/domain/coupon/service/CouponServiceImplTest.java
+++ src/test/java/com/team114/starbucks/domain/coupon/service/CouponServiceImplTest.java
@@ -1,12 +1,16 @@
 package com.team114.starbucks.domain.coupon.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +21,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.team114.starbucks.common.exception.BaseException;
 import com.team114.starbucks.domain.coupon.application.CouponServiceImpl;
+import com.team114.starbucks.domain.coupon.dto.in.CreateCouponReqDto;
+import com.team114.starbucks.domain.coupon.dto.out.CreateCouponResDto;
 import com.team114.starbucks.domain.coupon.dto.out.GetCouponResDto;
 import com.team114.starbucks.domain.coupon.entity.Coupon;
 import com.team114.starbucks.domain.coupon.enums.DiscountType;
@@ -71,6 +77,61 @@ public class CouponServiceImplTest {
 		assertThrows(BaseException.class, () -> couponService.findCouponByUuid(unvalidUuid));
 		verify(couponRepository).findByCouponUuid(unvalidUuid);
 		
+	}
+	
+	@Test
+	@DisplayName("쿠폰 저장에 성공한다.")
+	void saveCouponSuccess() {
+		
+		// given
+		CreateCouponReqDto dto = createCouponReqDto();
+		Coupon coupon = createCoupon(dto);
+		
+		when(couponRepository.save(any(Coupon.class))).thenReturn(coupon);
+		
+		// when
+		CreateCouponResDto result = couponService.saveCoupon(dto);
+		
+		// then
+		assertNotNull(result.getCouponUuid());
+		assertFalse(result.getCouponUuid().isBlank());
+		
+		assertEquals(coupon.getName(), result.getCouponName());
+		assertEquals(coupon.getDescription(), result.getCouponDescription());
+		assertEquals(coupon.getDiscountType(), result.getDiscountType());
+		assertEquals(coupon.getDiscountValue(), result.getDiscountValue());
+		assertEquals(coupon.getMinOrderPrice(), result.getMinOrderPrice());
+		assertEquals(coupon.getValidDays(), result.getValidDays());
+		
+		verify(couponRepository).save(any(Coupon.class));
+		
+	}
+
+	private CreateCouponReqDto createCouponReqDto() {
+		return CreateCouponReqDto.builder()
+				.couponName("coupon-name")
+				.couponDescription("coupon-description")
+				.discountType(DiscountType.DISCOUNT_TYPE_PERCENT)
+				.discountValue(50)
+				.minOrderPrice(12000)
+				.maxDiscountPrice(2000)
+				.validDays(30L)
+				.build();
+	}
+	
+	private Coupon createCoupon(CreateCouponReqDto dto) {
+		String couponUuid = "coupon-uuid";
+		return Coupon.builder()
+				.id(1L)
+				.couponUuid(couponUuid)
+				.name(dto.getCouponName())
+				.description(dto.getCouponDescription())
+				.discountType(dto.getDiscountType())
+				.discountValue(dto.getDiscountValue())
+				.minOrderPrice(dto.getMinOrderPrice())
+				.maxDiscountPrice(dto.getMaxDiscountPrice())
+				.validDays(dto.getValidDays())
+				.build();
 	}
 
 	private Coupon createCoupon() {
